c46f1f37459e34ffa1cf35e42e33b7409fdbf499
HG-ocsb <97174037+HG-ocsb@users.noreply.github.com>
2022-01-20 13:46:55 +0000

    test

--- UNIT 4/U4A1/main.py
+++ UNIT 4/U4A1/main.py
@@ -7,8 +7,13 @@
 # VARIABLE DEFINITION
 
 
-heleana
-
+number1 = 3
+number2 = 82
+number3 = 16
+number4 = 67
+number5 = 8
+print(number1 + number2 + number3 + number4 + number5)
+print(((number1 + number2 + number3 + number4 + number5)/3))
 
 # INPUT
 
