820b36d5c95a715f0a5f69b67b6b1878ff2b15c2
HG-ocsb <97174037+HG-ocsb@users.noreply.github.com>
2022-01-20 14:18:57 +0000

    Final work for U4A1

--- UNIT 4/U4A1/main.py
+++ UNIT 4/U4A1/main.py
@@ -1,30 +1,41 @@
-# NAME OF AUTHOR:  
-# NAME OF THE PROGRAM:  
+# NAME OF AUTHOR: Heleana 
+# NAME OF THE PROGRAM:Q4NUmbers and Q1Volume  
 # DATE OF CREATION:  
 # PURPOSE OF PROGRAM:  
 
 
 # VARIABLE DEFINITION
 
+                      #Q4NUmber
+# ask user for number
+print ('please enter firstnumber')
+firstnumber = int(input())
+print ('please enter secondnumber')
+secondnumber = int(input())
+print ('please enter thirdnumber')
+thirdnumber = int(input())
+print ('please enter fourthnumber')
+fourthnumber = int(input())
+print ('please enter fithnumber')
+fithnumber = int(input())
+#calculate sum
+print (firstnumber + secondnumber + thirdnumber + fourthnumber + fithnumber)
+#cal average
+print ((firstnumber + secondnumber + thirdnumber + fourthnumber + fithnumber)/5)
 
-number1 = 3
-number2 = 82
-number3 = 16
-number4 = 67
-number5 = 8
-print(number1 + number2 + number3 + number4 + number5)
-print(((number1 + number2 + number3 + number4 + number5)/3))
-
-# INPUT
-
-
-
-
-
-# PROCESSING
+                      #Q1Volume
+# ask user for number
+print ('please enter height')
+height = int(input())
+print ('please enter width')
+width = int(input())
+print ('please enter depth')
+depth = int(input())
+volume = (height*width*depth)
+print (volume)
 
 
 
 
 
-# OUTPUT
+
